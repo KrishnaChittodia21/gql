6d7a3e49ab8e666fad1c5b93523ade46ebd1e68a
KrishnaChittodia21 <krishnachittodia.10@gmail.com>
2021-01-06 03:56:48 +0530

    changes comment muatation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -97,7 +97,7 @@ const Mutation = {
     }
     return post;
   },
-  updateComment(parent, args, { db }, info) {
+  updateComment(parent, args, { db, pubsub }, info) {
     const { id, data } = args;
     const comment = db.comments.find((comment) => comment.id === id);
     if(!comment) {
@@ -106,6 +106,13 @@ const Mutation = {
     if(typeof data.text === 'string') {
       comment.text = data.text;
     }
+
+    pubsub.publish(`comment ${comment.post}`, {
+      comment: {
+        mutation: 'UPDATED',
+        data: comment
+      }
+    })
     return comment;
   },
   createComment(parent, args, { db, pubsub }, info) {
@@ -119,7 +126,12 @@ const Mutation = {
       ...args.data
     }
     db.comments.push(comment);
-    pubsub.publish(`comment ${args.data.post}`, { comment })
+    pubsub.publish(`comment ${args.data.post}`, {
+      comment: {
+        mutation: 'CREATED',
+        data: comment
+      }
+     })
     return comment;
   },
   deleteUser(parent, args, { db }, info) {
@@ -156,13 +168,19 @@ const Mutation = {
     }
     return post;
   },
-  deleteComment(parent, args, { db }, info) {
+  deleteComment(parent, args, { db, pubsub }, info) {
     const commentIndex = db.comments.findIndex((comment) => comment.id === args.id);
     if(commentIndex === -1) {
       throw new Error('Comment does not exists');
     }
-    const deletedComment = db.comments.splice(commentIndex, 1);
-    return deletedComment[0];
+    const [deletedComment] = db.comments.splice(commentIndex, 1);
+    pubsub.publish(`comment ${deletedComment.post}`, {
+      comment: {
+        mutation: 'DELETED',
+        data: deletedComment
+      }
+    })
+    return deletedComment;
   }
 }
 
